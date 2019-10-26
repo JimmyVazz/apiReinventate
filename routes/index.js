cd91a78c8589dff1517e32f82dfeb2c071a6fa08
JimmyVazz <cjvasquez_98@hotmail.com>
2019-10-26 10:13:34 -0500

    primer commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.json({
+    data: "ya jale we"
+  })
+})
+
+
+module.exports = router;
